aaafdcaba43e31a3d7e888b9dcc38831149f105d
gabrielynb <00001107548809sp@al.educacao.sp.gov.br>
2024-12-02 23:24:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é a capital do Brasil?',
+    'A capital do Brasil é Brasília.'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é a música mais ouvida no spotify?',
+    'A música mais ouvida no spotify é Blinding Lights, do cantor The Weeknd.'
+)
+
+criaCartao(
+    'Lingua Inglesa',
+    'Como se diz oi tudo bem em inglês?',
+    'Oi tudo bem em inglês é HI, HOW ARE YOU?'
+)
